82223fdb0617538d4083ef7e5d780ad6f4bab0ec
Ronan Sandoval <ronanchristophersandoval1@gmail.com>
2022-11-05 01:37:30 -0600

    being attack

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,8 @@ public class Player : MonoBehaviour
 
     public int[] currentRoom;
 
+    public GameObject attack;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +34,7 @@ public class Player : MonoBehaviour
     {
         move();   
         detectCurrentRoom();
+        detectAttack();
     }
 
     void move() {
@@ -43,5 +46,18 @@ public class Player : MonoBehaviour
     void detectCurrentRoom() {
         currentRoom[0] = (int)(transform.position.x / roomWidth);
         currentRoom[1] = (int)(transform.position.y / roomHeight);
+    }
+
+    void detectAttack() {
+        if (Input.GetMouseButtonDown(0)) {
+            // https://forum.unity.com/threads/get-vector-that-points-from-an-object-to-my-mouse.143980/
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = mousePos - transform.position;
+            direction.Normalize();
+
+            var pointAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Instantiate(attack, transform.position, Quaternion.LookRotation(Vector3.forward, mousePos - transform.position));
+
+        }
     }
 }
